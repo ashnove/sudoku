4935c6a3cb6d59521495ff1ccad1bd52c05d2ab1
ashnove <55032392+ashnove@users.noreply.github.com>
2020-07-14 21:21:56 +0530

    added comment lines to explain the parts

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import time  
+#sample board
 board = [[6,5,0,3,0,0,0,0,0],
          [0,0,4,0,0,0,3,0,1],
          [0,0,0,2,0,8,0,0,5],
@@ -9,6 +10,7 @@ board = [[6,5,0,3,0,0,0,0,0],
          [5,0,6,0,0,0,8,0,0],
          [0,0,0,0,0,2,0,3,9]]
 
+#bactrack algo lies in here
 def solve(bo):
     find = find_empty(bo)
     if not find:
@@ -21,7 +23,7 @@ def solve(bo):
             bo[row][col] = i
             #print("\n\n")
             #print_board(bo)
-            if solve(bo):
+            if solve(bo): #this is the function call jaha baar baar har value uss cell me daal ke dekh rahe if it is valid or not..cool?
                 return True
 
             bo[row][col] = 0
@@ -29,7 +31,7 @@ def solve(bo):
     return False
 
 
-
+#just displaying the borad zyada mat sochna yaha
 def print_board(bo):
     for i in range(len(bo)):
 
@@ -47,6 +49,7 @@ def print_board(bo):
                 #time.sleep(0.1)
                 print(str(bo[i][j]) + " ",end="")
 
+# cell with 0 in it is the empty cell ..
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
@@ -57,14 +60,16 @@ def find_empty(bo):
 
 def valid(bo, num , pos):
     
+    # here chekcking refers to if the number satisfies the condition for row, col, and the box... rules nai pata toh google ker lo
+    #check row 
     for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
             return False
-
+    #check column
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
-
+    #check the box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
@@ -76,9 +81,9 @@ def valid(bo, num , pos):
     return True
 
 
-print_board(board)
-solve(board)
-
+print_board(board)# printing unsolved board
+solve(board)#solving it
+#then print the solved board or show the error message if the root board was valid or not...
 if not find_empty(board):
     print("\n\nSolved board:\n\n")
     print_board(board)
